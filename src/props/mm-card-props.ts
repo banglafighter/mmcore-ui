abc614958a7053fa91e772152a402972b395bfde
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-04-10 00:37:01 +0600

    Working on UI

---
+++ src/props/mm-card-props.ts
@@ -0,0 +1,40 @@
+import {MMDefaultProps} from "../common/mm-default-props";
+
+
+export interface DefaultCardProps extends MMDefaultProps {}
+
+export interface DefaultCardHeaderProps extends MMDefaultProps {}
+
+export interface DefaultCardFooterProps extends MMDefaultProps {}
+
+export interface DefaultCardTitleProps extends MMDefaultProps {}
+
+export interface DefaultCardSubTitleProps extends MMDefaultProps {}
+
+export interface DefaultCardActionProps extends MMDefaultProps {}
+
+
+export interface WebCardProps extends DefaultCardProps {}
+
+export interface WebCardHeaderProps extends DefaultCardHeaderProps {}
+
+export interface WebCardFooterProps extends DefaultCardFooterProps {}
+
+export interface WebCardTitleProps extends DefaultCardTitleProps {}
+
+export interface WebCardSubTitleProps extends DefaultCardSubTitleProps {}
+
+export interface WebCardActionProps extends DefaultCardActionProps {}
+
+
+export interface AppCardProps extends DefaultCardProps {}
+
+export interface AppCardHeaderProps extends DefaultCardHeaderProps {}
+
+export interface AppCardFooterProps extends DefaultCardFooterProps {}
+
+export interface AppCardTitleProps extends DefaultCardTitleProps {}
+
+export interface AppCardSubTitleProps extends DefaultCardSubTitleProps {}
+
+export interface AppCardActionProps extends DefaultCardActionProps {}
