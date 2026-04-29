421dc7165f32da9a49566fcdae0ab6365d86eec0
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-04-29 11:46:52 +0600

    Working UI Exp

---
+++ src/props/mm-form-props.ts
@@ -0,0 +1,4 @@
+
+
+// Props: submitUrl, method: [post/get/upload/form], submitSuccess(), submitFailed(), beforeSubmit(), onError(), beforeValidation(), title, subtitle, onCancel, submitLabel, otherButtons, layout = Grid
+// NOTE: Conditional show Hide Input, conditional load dropdown
