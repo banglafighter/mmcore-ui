fb536acb32531369625374fe403dab3981f1942b
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-15 23:51:03 +0600

    Working on UI System

---
+++ src/props/mm-file-field-props.ts
@@ -0,0 +1,17 @@
+import {WebDefaultInputFieldPropsBase} from "mmcore-ui";
+import {UIComponentProps, UINode} from "mmcore";
+
+export interface FileFieldAcceptFile {
+    [key: string]: readonly string[];
+}
+
+export interface WebFileFieldPropsBase extends WebDefaultInputFieldPropsBase {
+    minSize?: number
+    maxSize?: number
+    maxFiles?: number
+    multiple?: boolean
+    acceptFile?: FileFieldAcceptFile
+    centerContent?: UINode
+}
+
+export type WebFileFieldProps = WebFileFieldPropsBase & UIComponentProps<"input">
