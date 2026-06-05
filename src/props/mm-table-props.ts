bc888a521c797f6d7a29b89fbce2e8ad1bd04a00
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-06-05 19:43:02 +0600

    Updating various things

--- src/props/mm-table-props.ts
+++ src/props/mm-table-props.ts
@@ -74,4 +74,5 @@ export type WebTableGeneratorProps = WebTableGeneratorPropsBase & UIComponentPro
 export interface WebTableEngineProps extends DefaultTableEngineProps {
     registerColumns(columns: (columns: WebTableGeneratorColumnProps[]) => WebTableGeneratorColumnProps[]): WebTableGeneratorColumnProps[]
     getColumns: () => WebTableGeneratorColumnProps[]
+    addDynamicColumns: (columns: (columns: WebTableGeneratorColumnProps[]) => WebTableGeneratorColumnProps[]) => void
 }
