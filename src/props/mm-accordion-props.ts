75aac4e048ca6b812c67fe469fcb2e19877f1f15
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-06-16 20:27:12 +0600

    updating various things

---
+++ src/props/mm-accordion-props.ts
@@ -0,0 +1,31 @@
+import {MMDefaultProps} from "../common/mm-default-props"
+import {UIComponentProps, UINode} from "mmcore";
+
+export type AccordionSelectionType = "single" | "multiple"
+
+export interface DefaultAccordionItemProps extends MMDefaultProps {
+    accordionId?: string
+    labelContent: UINode
+    component: UINode
+    isHidden?: boolean
+    isDisabled?: boolean
+}
+
+export interface DefaultAccordionProps extends MMDefaultProps {
+    selectType?: AccordionSelectionType
+    defaultAccordionId?: string
+    items: unknown[]
+}
+
+export interface WebAccordionItemPropsBase extends DefaultAccordionItemProps {
+    componentClassName?: string
+    itemClassName?: string
+}
+
+export type WebAccordionItemProps = WebAccordionItemPropsBase & UIComponentProps<"div">
+
+export interface WebAccordionPropsBase extends DefaultAccordionProps {
+    items: WebAccordionItemProps[]
+}
+
+export type WebAccordionProps = WebAccordionPropsBase & UIComponentProps<"div">
