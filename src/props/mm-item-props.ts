659aeb580c7d59a4b796d2d28f1e2da743b32ab2
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-06-21 22:16:42 +0600

    Added Item

---
+++ src/props/mm-item-props.ts
@@ -0,0 +1,41 @@
+import {MMDefaultProps} from "../common/mm-default-props";
+import {UIComponentProps} from "mmcore";
+
+export type ItemVariation = "default" | "outline" | "muted"
+export type ItemSize = "default" | "sm"
+export type ItemMediaVariation = "default" | "icon" | "image"
+
+export interface DefaultItemProps extends MMDefaultProps {
+    variant?: ItemVariation
+    size?: ItemSize
+}
+
+export interface DefaultItemBodyProps extends MMDefaultProps {}
+
+export interface DefaultItemTitleProps extends MMDefaultProps {}
+
+export interface DefaultItemSubTitleProps extends MMDefaultProps {}
+
+export interface DefaultItemActionProps extends MMDefaultProps {}
+
+export interface DefaultItemMediaProps extends MMDefaultProps {
+    variant?: ItemMediaVariation
+}
+
+export interface WebItemPropsBase extends DefaultItemProps {}
+export type WebItemProps = WebItemPropsBase & UIComponentProps<"div">
+
+export interface WebItemBodyPropsBase extends DefaultItemBodyProps {}
+export type WebItemBodyProps = WebItemBodyPropsBase & UIComponentProps<"div">
+
+export interface WebItemTitlePropsBase extends DefaultItemTitleProps {}
+export type WebItemTitleProps = WebItemTitlePropsBase & UIComponentProps<"div">
+
+export interface WebItemSubTitlePropsBase extends DefaultItemSubTitleProps {}
+export type WebItemSubTitleProps = WebItemSubTitlePropsBase & UIComponentProps<"p">
+
+export interface WebItemActionPropsBase extends DefaultItemActionProps {}
+export type WebItemActionProps = WebItemActionPropsBase & UIComponentProps<"div">
+
+export interface WebItemMediaPropsBase extends DefaultItemMediaProps {}
+export type WebItemMediaProps = WebItemMediaPropsBase & UIComponentProps<"div">
