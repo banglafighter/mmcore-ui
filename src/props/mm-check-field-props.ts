684d06d712ecb13d7a56ac29661eecced87c7f38
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-07-02 00:18:45 +0600

    Allow custom value send

--- src/props/mm-check-field-props.ts
+++ src/props/mm-check-field-props.ts
@@ -1,10 +1,12 @@
 import {WebDefaultInputFieldPropsBase} from "./mm-input-common-props";
 import {UIComponentProps} from "mmcore";
+import {FieldValueType} from "./mm-field-engine-props";
 
 export type CheckFieldType = "checkbox" | "switch";
 
 export interface WebCheckFieldPropsBase extends WebDefaultInputFieldPropsBase {
     type?: CheckFieldType
+    sendValue?: FieldValueType
 }
 
-export type WebCheckFieldProps = WebCheckFieldPropsBase & UIComponentProps<"input">
+export type WebCheckFieldProps = WebCheckFieldPropsBase &  Omit<UIComponentProps<"input">, "defaultValue">;
